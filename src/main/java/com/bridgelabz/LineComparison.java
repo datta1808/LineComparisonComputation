69bd52c910a20daafe206e0f8eda9bc6dc1a00c0
datta1808 <bagaledatta18@gmail.com>
2021-05-21 21:42:35 +0530

    REFACTOR: modified code using OOP concept

--- src/main/java/com/bridgelabz/LineComparison.java
+++ src/main/java/com/bridgelabz/LineComparison.java
@@ -1,31 +1,47 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class LineComparison {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison Computation");
 
-        // Initializing the co-ordinates for line 1
-        int x1=1, x2=4, y1=1, y2=4;
-        // Initializing the co-ordinates for line 2
-        int a1=1, a2=5, b1=1, b2=5;
+    double x1, x2, y1, y2;
+    Double lengthOfLine;
+    Double line1, line2;
+    public void values() {
+        System.out.println("Enter values of points: x1, x2, y1, y2");
+        // user inputs
+        Scanner point = new Scanner(System.in);
+        x1 = point.nextDouble();
+        x2 = point.nextDouble();
+        y1 = point.nextDouble();
+        y2 = point.nextDouble();
+        System.out.println("The co-ordinates are: (" + x1 + ", " + y1 + ")" + "(" + x2 + "," + y2 + ")");
+    }
 
-        // Calculating the length of line
-        Double lengthOfLine1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        Double lengthOfLine2 = Math.sqrt(Math.pow(a2 - a1, 2) + Math.pow(b2 - b1, 2));
+    // method to calculate length of the line
+    public double lengthCalculation() {
+        lengthOfLine = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        return lengthOfLine;
+    }
 
-        // Printing the length of lines
-        System.out.println("The length of line 1 is: " +lengthOfLine1);
-        System.out.println("The length of line 2 is: " +lengthOfLine2);
+    // method to check equality of lines
+    public void EqualityCheck() {
+        System.out.println("For line 1: ");
+        values();
+        lengthCalculation();
+        line1 = lengthOfLine;
+        System.out.println("length of line 1 = " + line1);
 
-        // Comparing the lengths of the lines using 'equals' method
-        if (lengthOfLine1.equals(lengthOfLine2)) {
-            System.out.println("Both the lines are equal");
-        } else {
-            System.out.println("Both the lines are not equal");
-        }
+        System.out.println("For line 2: ");
+        values();
+        lengthCalculation();
+        line2 = lengthOfLine;
+        System.out.println("length of line 2 = " + line2);
+    }
 
-        //Comparing the lengths of lines using 'compareTo' metthod
-        int lengthCheck = lengthOfLine1.compareTo(lengthOfLine2);
+    // method to compare the length of lines
+    public void Compare() {
+        int lengthCheck = line1.compareTo(line2);
         switch (lengthCheck) {
             case 0:
                 System.out.println("Line 1 is equal to line 2");
@@ -37,5 +53,12 @@ public class LineComparison {
                 System.out.println("Line 2 is greater than line 1");
                 break;
         }
+    }
+
+    public static void main(String[] args) {
+        //Object creation
+        LineComparison lines = new LineComparison();
+            lines.EqualityCheck();
+            lines.Compare();
     }
 }
